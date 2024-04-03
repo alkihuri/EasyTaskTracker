9f7f9115b7d19f6fc307293697261b9726fda705
alkihuri <alkihuri@gmail.com>
2024-04-03 05:36:42 +0200

    Auth bug fixed

--- EasyTaskTracker/EasyTrackerAPI/Model/Implementation/AccountManager.cs
+++ EasyTaskTracker/EasyTrackerAPI/Model/Implementation/AccountManager.cs
@@ -15,11 +15,30 @@ public class AccountManager : IAccountManager
 
     public void RegisterAccount(User account)
     {
-        Console.WriteLine("[Account manager] Creating account: " + account.Name);
+        Console.WriteLine("[Account manager] registring account: " + account.Name);
 
         if(_context.Users.Any(u => u.Name == account.Name))
         {
             Console.WriteLine("Account with name " + account.Name + " already exists."); 
+            return;
+        }
+
+        string [] domens = new string[3]{"ru","com","com"};
+
+
+        if(account.Name.Contains("@") 
+            && account.Name.Contains(".") 
+            && account.Name.Length > 5
+            && account.Name.Length < 50
+            && domens.Any(d => account.Name.Split('.').Last().Contains(d)))   
+        {    
+            account.Email = account.Name;  
+            account.Name = account.Name.Split('@').First();
+            Console.WriteLine($"Email of {account.Name} is valid."); 
+        }
+        else
+        {
+            account.Email = account.Name;   
         }
 
         account.ID = _context.Users.Count() + 1;    
